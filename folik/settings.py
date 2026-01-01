fbd1a993c3b56baa2dccffcb1e2efc2ac62782cd
klokc228-ai <klokc228@gmail.com>
2026-01-01 23:37:21 +0300

    Deploy commit

--- folik/settings.py
+++ folik/settings.py
@@ -85,7 +85,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # ── MEDIA FILES ──
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # ── DEFAULT AUTO FIELD ──
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
