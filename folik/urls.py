c946376f4b39342763d2c3b7963b14ce6acdb774
Kirill Chumak <_@github.com>
2026-01-29 07:18:34 +0200

    Fix i18n structure and templates, prepare locale for translations

--- folik/urls.py
+++ folik/urls.py
@@ -6,6 +6,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),  # <-- подключаем смену языка
+
     
 ]
 
